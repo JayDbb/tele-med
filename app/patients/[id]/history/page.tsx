3e15d7e5994564e71935c50875d159a4de0d39bd
JayDbb <justin670brown@gmail.com>
2026-01-05 12:01:49 -0500

    fix patient page error from next 15 -> 16 conversion

--- app/patients/[id]/history/page.tsx
+++ app/patients/[id]/history/page.tsx
@@ -3,17 +3,18 @@ import PatientDetailSidebar from '@/components/PatientDetailSidebar'
 import VisitHistory from '@/components/VisitHistory'
 import GlobalSearchBar from '@/components/GlobalSearchBar'
 
-export default function VisitHistoryPage({ params }: { params: { id: string } }) {
+export default async function VisitHistoryPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params
   return (
     <div className="relative flex min-h-screen w-full">
       <RoleBasedSidebar />
-      <PatientDetailSidebar patientId={params.id} />
+      <PatientDetailSidebar patientId={id} />
 
       <main className="flex-1 p-8">
         <div className="mb-6">
           <GlobalSearchBar />
         </div>
-        <VisitHistory patientId={params.id} />
+        <VisitHistory patientId={id} />
       </main>
     </div>
   )
