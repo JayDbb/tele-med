53c8a3ee32a8b0fbbccd0b0a61b68c963737edfe
JayDbb <justin670brown@gmail.com>
2026-01-05 13:19:51 -0500

    fix next 16 error on /new-visit

--- app/patients/[id]/new-visit/page.tsx
+++ app/patients/[id]/new-visit/page.tsx
@@ -1,5 +1,6 @@
 import NewVisitForm from '@/components/NewVisitForm'
 
-export default function NewVisitPage({ params }: { params: { id: string } }) {
-  return <NewVisitForm patientId={params.id} />
+export default async function NewVisitPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params
+  return <NewVisitForm patientId={id} />
 }
